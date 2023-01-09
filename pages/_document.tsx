149e2af8c66191e534b68c7ec585f431985609f4
Arthur <peraarthur2018@gmail.com>
2023-01-09 14:48:48 -0300

    stiche ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet"></link>
+        
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$purple'
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Button>Hello world</Button>
     </>
   )
 }
