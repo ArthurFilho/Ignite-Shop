f3ae6052fd145ab7a4551239cd1e82d75ab85396
Arthur <peraarthur2018@gmail.com>
2023-01-09 16:04:15 -0300

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
+import { GlobalStyles } from '../styles/global'
+
+GlobalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+
+
   return <Component {...pageProps} />
 }
