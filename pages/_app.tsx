a66b6a2b41fc659b229771f0d8f1cac331bb475a
Arthur <peraarthur2018@gmail.com>
2023-02-03 21:58:59 -0300

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from '../styles/global'
 
-GlobalStyles();
+import Logo from '../assets/Logo.svg'
+import { Container, Header } from '../styles/pages/app';
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  GlobalStyles();
 
+  console.log(Logo)
 
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={Logo.src} />
+      </Header>
+
+    <Component {...pageProps} />
+    </Container>
+  )
 }
